abb832d515583bb7c54766c7f6e8214ff812687e
Shashwat Verma <shashwatv18@gmail.com>
2024-01-05 01:36:19 +0530

    implemented bcrypt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,50 +1,47 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
-exports.addUser = (req, res, next) => {
+exports.addUser = async (req, res, next) => {
 
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
 
-    User
-        .create({
-            name: name,
-            email: email,
-            password: password
-        })
-        .then(result => {
-            res.json({ "message": "User registered successfully !", check: true });
+    const hashedPassword = await bcrypt.hash(password,10);
 
-        })
-        .catch(err => {
-            res.json({ "message": "User already registered !", check: false })
-        });
+    console.log(hashedPassword);
+
+    try{
+        const user = await User.create({name, email, password: hashedPassword});
+        res.json({ "message": "User registered successfully !", check: true });
+    }catch(err){
+        res.json({ "message": "User already registered !", check: false })
+    }
 
 };
 
-exports.loginUser = (req, res, next) => {
+exports.loginUser = async (req, res, next) => {
 
     const email = req.body.email;
     const password = req.body.password;
 
-    User.findAll({where : {email: email}})
-        .then(user => {
+    try{
+        const user = await User.findOne({where : {email}});
 
-            if(Object.keys(user).length === 0){
-                res.status(404).json({message : "User not found!", display : "User is not registered !"});
-            }else{
-                const passwordSaved = user[0].dataValues.password;
-
-                if(password === passwordSaved){
-                    res.status(200).json({message : "User Login Successfully !", display: ""});
-                }
-                else{
-                    res.status(401).json({message: "User not authorized", display : "Incorrect email or password"});
-                }
+        if(!user){
+            res.status(404).json({message : "User not found!", display : "User is not registered !"});
+        }else{
+            
+            if(await bcrypt.compare(password, user.password)){
+                res.status(200).json({message : "User Login Successfully !", display: ""});
             }
-        })
-        .catch(err => console.log(err));
+            else{
+                res.status(401).json({message: "User not authorized", display : "Incorrect email or password"});
+            }
+        }
 
-
+    }catch(err){
+        console.log(err);
+    }
 
 }
